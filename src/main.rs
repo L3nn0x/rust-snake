88a60ee225f0fb0befc3c97e7ebcf5d6ba7124a0
L3nn0x <dragon83.super@gmail.com>
2018-04-28 19:14:16 +0100

    Added state machine

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,14 @@ struct Game {
 }
 
 impl Game {
+    fn generate_apple(&mut self) {
+        use rand::Rng;
+        let mut rng = rand::thread_rng();
+        self.apple = (rng.gen_range(0, 600 / 20), rng.gen_range(0, 400 / 20));
+    }
+}
+
+impl State for Game {
     fn render(&mut self, arg: &RenderArgs) {
         use graphics;
 
@@ -53,19 +61,14 @@ impl Game {
         });
     }
 
-    fn generate_apple(&mut self) {
-        use rand::Rng;
-        let mut rng = rand::thread_rng();
-        self.apple = (rng.gen_range(0, 600 / 20), rng.gen_range(0, 400 / 20));
-    }
-
-    fn update(&mut self) {
+    fn update(&mut self, u: &UpdateArgs) -> Transition {
         if self.snake.update(&self.apple) {
             self.generate_apple();
         }
+        Transition::None
     }
 
-    fn pressed(&mut self, btn: &Button) {
+    fn handle_event(&mut self, btn: &Button) -> Transition {
         let last_dir = self.snake.dir.clone();
 
         self.snake.dir = match *btn {
@@ -75,6 +78,7 @@ impl Game {
             Button::Keyboard(Key::Right) if last_dir != Direction::Left => Direction::Right,
             _ => last_dir,
         };
+        Transition::None
     }
 }
 
@@ -135,13 +139,140 @@ impl Snake {
     }
 }
 
+enum Transition {
+    None,
+    Pop,
+    Push(Box<State>),
+    Switch(Box<State>),
+    Quit
+}
+
+trait State {
+    fn render(&mut self, args: &RenderArgs);
+    fn update(&mut self, args: &UpdateArgs) -> Transition;
+    fn handle_event(&mut self, btn: &Button) -> Transition;
+    fn on_start(&mut self) {}
+    fn on_stop(&mut self) {}
+    fn on_pause(&mut self) {}
+    fn on_resume(&mut self) {}
+}
+
+struct StateMachine {
+    running: bool,
+    states: Vec<Box<State>>
+}
+
+impl StateMachine {
+    fn new(state: Box<State>) -> StateMachine {
+        StateMachine {
+            running: false,
+            states: vec![state]
+        }
+    }
+
+    fn is_running(&self) -> bool {
+        self.running
+    }
+
+    fn render(&mut self, args: &RenderArgs) {
+        if self.running {
+            let state = self.states.last_mut().unwrap();
+            state.render(args);
+        }
+    }
+
+    fn start(&mut self) {
+        if !self.running {
+            let state = self.states.last_mut().unwrap();
+            state.on_start();
+            self.running = true;
+        }
+    }
+
+    fn handle_event(&mut self, btn: &Button) {
+        if self.running {
+            let trans = match self.states.last_mut() {
+                Some(ref mut state) => state.handle_event(btn),
+                None => Transition::None
+            };
+            self.transition(trans);
+        }
+    }
+
+    fn update(&mut self, u: &UpdateArgs) {
+        if self.running {
+            let trans = match self.states.last_mut() {
+                Some(ref mut state) => state.update(u),
+                None => Transition::None
+            };
+            self.transition(trans);
+        }
+    }
+
+    fn transition(&mut self, trans: Transition) {
+        if self.running {
+            match trans {
+                Transition::None => {},
+                Transition::Pop => self.pop(),
+                Transition::Push(state) => self.push(state),
+                Transition::Switch(state) => self.switch(state),
+                Transition::Quit => self.stop()
+            }
+        }
+    }
+
+    fn switch(&mut self, state: Box<State>) {
+        if self.running {
+            if let Some(mut state) = self.states.pop() {
+                state.on_stop();
+            }
+            self.states.push(state);
+            let state = self.states.last_mut().unwrap();
+            state.on_start();
+        }
+    }
+
+    fn push(&mut self, state: Box<State>) {
+        if self.running {
+            if let Some(ref mut state) = self.states.last_mut() {
+                state.on_pause();
+            }
+            self.states.push(state);
+            let state = self.states.last_mut().unwrap();
+            state.on_start();
+        }
+    }
+
+    fn pop(&mut self) {
+        if self.running {
+            if let Some(mut state) = self.states.pop() {
+                state.on_stop();
+            }
+            if let Some(ref mut state) = self.states.last_mut() {
+                state.on_resume();
+            } else {
+                self.running = false;
+            }
+        }
+    }
+
+    fn stop(&mut self) {
+        if self.running {
+            while let Some(mut state) = self.states.pop() {
+                state.on_stop();
+            }
+            self.running = false;
+        }
+    }
+}
+
 fn main() {
     let opengl = OpenGL::V3_2;
 
     let mut window: GlutinWindow = WindowSettings::new("Snake Game", [600, 400])
                                     .opengl(opengl).exit_on_esc(true).build().unwrap();
     
-    let mut game = Game {
+    let game = Game {
         gl: GlGraphics::new(opengl),
         snake: Snake { 
             body: LinkedList::from_iter((vec![(0,0), (0,1)]).into_iter()),
@@ -149,19 +280,23 @@ fn main() {
         apple: (3, 3)
     };
 
+
+    let mut state_machine = StateMachine::new(Box::new(game));
+    state_machine.start();
+
     let mut events = Events::new(EventSettings::new()).ups(8);
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
-            game.render(&r);
+            state_machine.render(&r);
         }
 
         if let Some(u) = e.update_args() {
-            game.update();
+            state_machine.update(&u);
         }
 
         if let Some(k) = e.button_args() {
             if k.state == ButtonState::Press {
-                game.pressed(&k.button);
+                state_machine.handle_event(&k.button);
             }
         }
     }
